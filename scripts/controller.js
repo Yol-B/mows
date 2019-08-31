1d95c79ee504bb9562064feb8e38fd61e5fa400d
1styrGroupB <1styrGroupB@pn.local>
2019-08-31 11:50:13 +0800

    semantic ui added

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,14 +1,12 @@
 
-$(document).ready(function () {
-  $('body').bootstrapMaterialDesign();
+$(document).ready(function() {
   var client = null;
-  $(".btn-disconnect").attr("disabled", true);
-  // $(".btn-unsubscribe").attr("disabled", true);
+  var connected= false;
+  var topics = []
 
-  $(".btn-connect").click(function () {
+  $(".btn-connect").click(function() {
     var address = $("#address").val();
     var timestamp = null;
-
     if (address == "") {
       Swal.fire({
         type: 'error',
@@ -16,70 +14,100 @@ $(document).ready(function () {
         text: 'Please add a broker address to connect..',
       });
     } else {
-      connect = true;
-      client = mqtt.connect(address);
-      $(".status").text("connecting...")
-      client.on("connect", function () {
-        Swal.fire({
-          type: 'success',
-          title: 'Connected Successfilly!'
-        });
-        $(".status-bar").removeClass("alert-secondary");
-        $(".status-bar").addClass("alert-success");
-        $(".status").text("connected")
-        $(".btn-disconnect").attr("disabled", false);
-      })
+      if (!connected) {
+        client = mqtt.connect(address);
+        $(".status").text("connecting...")
+        client.on("connect", function() {
+          Swal.fire({
+            type: 'success',
+            title: 'Connected Successfilly!'
+          });
+          $("#conBtn").removeClass("btn-connect positive")
+          $("#conBtn").addClass("wifi btn-disconnect negative")
+          $("#text").text("Disconnect")
+          $(".status").text("Connected")
+          $(".message").addClass("green")
+          connected = true;
+        })
+      } else {
+        $(this).removeClass("wifi btn-disconnect negative")
+        $(this).addClass("btn-connect positive")
+        $("#text").text("Connect")
+        $(".status").text("Disconnected")
+        $(".message").removeClass("green")
+      }
     }
 
-    $(".btn-disconnect").click(function () {
+    $(".btn-disconnect").click(function() {
+      connected = false;
+      $(this).removeClass("wifi btn-disconnect negative")
+      $(this).addClass("btn-connect positive")
+      $("#text").text("Connect")
       client.end();
-      $(".status-bar").removeClass("alert-success");
-      $(".status-bar").addClass("alert-secondary");
-      $(".status").text("Disconnected")
-      $(".btn-disconnect").attr("disabled", true);
     });
 
-    $(".btn-publish").click(function () {
-      var my_payload = $("#payload").val();
-      var my_topic = $("#topic").val();
-      if (my_payload == "" || my_topic == "") {
-        Swal.fire({
-          type: 'error',
-          title: 'Oops...',
-          text: 'Please add a Topic or/and a Payload!',
-        });
-      } else {
-        client.publish(my_topic, my_payload)
-        timestamp = moment().format('MMMM D YYYY , h:mm:ss a')
-        Swal.fire({
-          type: 'success',
-          title: 'Success...',
-          text: 'Topic Successfully Published!',
-        });
+    $(".btn-publish").click(function() {
+      if (connected) {
+        var my_payload = $("#payload").val();
+        var my_topic = $("#topic").val();
+        if (my_payload == "" || my_topic == "") {
+          Swal.fire({
+            type: 'error',
+            title: 'Oops...',
+            text: 'Please add a Topic or/and a Payload!',
+          });
+        } else {
+          client.publish(my_topic, my_payload)
+          timestamp = moment().format('MMMM D YYYY , h:mm:ss a')
+          Swal.fire({
+            type: 'success',
+            title: 'Success...',
+            text: 'Topic Successfully Published!',
+          });
+        }
+      }
+
+    });
+    //   <div class="item" data-value="zw"><i class="zw flag"></i>Zimbabwe</div>
+    // .menu
+    $(".btn-subscribe").click(function() {
+      if (!connected) {
+        var topic_s = $("#topic_s").val();
+        if (topic_s == "") {
+          Swal.fire({
+            type: 'error',
+            title: 'Oops...',
+            text: 'Please add a Topic to Subscribe',
+          });
+        } else {
+          if (topics.includes(topic_s)) {
+            Swal.fire({
+              type: 'info',
+              title: 'Topic already subscribed'
+            });
+          } else {
+            topics.push(topic_s);
+            for (let i = 0; i < topics.length; i++) {
+              var opt = new Option(topics[i], topics[i])
+              $('select').append($(opt).attr("class", "item"));
+              
+            }
+            
+            Swal.fire({
+              type: 'success',
+              title: 'Success...',
+              text: topic_s + ' Successfully Subscribed!',
+            });
+            console.log("subscribed")
+            client.subscribe(topic_s);
+          }
+        }
       }
     });
 
-    $(".btn-subscribe").click(function () {
-      var topic_s = $("#topic_s").val();
-      if (topic_s == "") {
-        Swal.fire({
-          type: 'error',
-          title: 'Oops...',
-          text: 'Please add a Topic to Subscribe',
-        });
-      } else {
-        Swal.fire({
-          type: 'success',
-          title: 'Success...',
-          text: topic_s + ' Successfully Subscribed!',
-        });
-        // $(".btn-unsubscribe").attr("disabled", false);
+    $(".btn-unsubscribe").click(function() {})
 
-        client.subscribe(topic_s);
-      }
-    });
-
-    client.on("message", function (topic, payload) {
+    client.on("message", function(topic, payload) {
       var tr = $("<tr>")
       $("<td>").text(topic).appendTo($(tr))
       $("<td>").text(payload).appendTo($(tr))
@@ -89,34 +117,4 @@ $(document).ready(function () {
       console.log([topic, payload].join(": "));
     })
   })
-
-
-
-
-  // ws://broker.hivemq.com:8000/mqtt
-
-
-
-
-
-  // // advance functionalities
-  // client = mqtt.connect("ws://broker.hivemq.com:8000/mqtt")
-  // client.subscribe("mqtt/demo")
-
-  // client.on("connect", function(){
-  //     console.log("Successfully connected");
-  // })
-
-  // client.on("message", function (topic, payload) {
-  //   console.log([topic, payload].join(": "));
-  //   client.end();
-  // })
-
-  // client.publish("mqtt/demo", "hello world!", function(err){
-  //   if (err){
-  //     console.log(err)
-  //   } else {
-  //     console.log("published")
-  //   }
-  // })
 });
